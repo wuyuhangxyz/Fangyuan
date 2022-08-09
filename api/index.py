ea29c454ea054c20fccfc37f92b85259d622e962
wuyuhangxyz <99400510+wuyuhangxyz@users.noreply.github.com>
2022-08-09 21:21:42 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,6 @@
-from http.server import BaseHTTPRequestHandler
- 
- 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write("DearXuan's API by python!".encode())
-        return
+from flask import *
+
+app = Flask()
+@app.route("/")
+def index():
+    return "This is Flask API test!"
